04422052cbff6b6081fa9497eefed05b8dac2546
paoyingheng <44899774+paoyingheng@users.noreply.github.com>
2019-04-19 23:24:13 +0800

    Add files via upload

--- plot3.R
+++ plot3.R
@@ -1,31 +1,38 @@
+#Downloads file from target URL
+data_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(data_url, "power.zip")
+
+#Unzips downloaded file into folder called 'power'
+unzip("power.zip", exdir="power")
+
+#Reads the txt file, while addressing missing values, limiting the rows read to the last row with Feb 2nd 2007 data, as well as specifying column classes
 power <- read.table("~/power/household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", nrows=69516, colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
 
-#selecting only rows pertaining to Feb 1st 2007 to Feb 2nd 2007
+#select only rows pertaining to Feb 1st 2007 to Feb 2nd 2007
 power1 <- power[66637:69516,]
-
 
 #Format date using as.Date
 power1$Date <- as.Date(power1$Date, "%d/%m/%Y")
 
-#Selecting rows which are complete
+#Select only rows which are complete
 power1 <- power1[complete.cases(power1),]
 
-#Combining date and time columns
+#Combine the Date and Time columns
 datetime <- paste(power1$Date, power1$Time)
 
-#Naming the combined column
+#Name the combined column
 datetime <- setNames(datetime, "DateTime")
 
-#Removing the original Date and Time columns from the power1 dataset
+#Remove the original Date and Time columns from the power1 dataset
 power1 <- power1[ ,!(names(power1) %in% c("Date","Time"))]
 
-#Adding the new column (datetime) into the power1 dataset
+#Add the new column (datetime) into the power1 dataset
 power1 <- cbind(datetime, power1)
 
-#Formatting the datetime column in the updated power1 dataset
+#Format the datetime column in the updated power1 dataset
 power1$datetime <- as.POSIXct(datetime)
 
-##plot 3
+##Plot and save plot 3
 png("plot3.png", width = 480, height = 480)
 with(power1, {
   plot(datetime, Sub_metering_1, type="l", ylab="Energy sub metering", xlab=" ")
